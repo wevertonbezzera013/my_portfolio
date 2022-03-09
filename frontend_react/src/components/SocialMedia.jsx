5361d0f98aff636d42b085168117c94c47828b95
Weverton <wevertonbezzera013@gmail.com>
2022-03-09 18:26:00 -0300

    details

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
-import { BsTwitter, BsInstagram, BsLinkedin } from 'react-icons/bs'
+import { BsTwitter, BsInstagram, BsLinkedin, BsGithub } from 'react-icons/bs'
 
 const SocialMedia = () => {
     return (
     <div className="app__social">
         <div>
-            <BsTwitter/>
+            <a href="https://mobile.twitter.com/wev_dev"><BsTwitter/></a>
         </div>
         <div>
-            <BsInstagram/>
+            <a href="https://instagram.com/wevertonbezzera?utm_medium=copy_link"><BsInstagram/></a>
         </div>
         <div>
-            <BsLinkedin/>
+            <a href="https://www.linkedin.com/in/weverton-bezerra-da-costa-061ab11a4/"><BsLinkedin/></a>
+        </div>
+        <div>
+            <a href="https://github.com/wevertonbezzera013"><BsGithub/></a>
         </div>
     </div>
     )
